1839e5647c011927e8004c74fd72156715c82c6e
keshavwadhwa573 <keshavwadhwa573@gmail.com>
2026-07-18 03:32:30 +0530

    connect to MONGODB

---
+++ src/index.js
@@ -0,0 +1,50 @@
+//require('dotenv').config({path : './env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+dotenv.config({
+    path : './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//this can be done but we are connecting in db folder
+// import mongoose, { connect } from "mongoose";
+// import {DB_NAME} from "./constants"
+// import express from "express";
+// import { useSyncExternalStore } from "react";
+// const app=express();
+
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("ERROR : ",error);
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`app is listening an=t port number ${process.env.PORT}`)
+//         })
+//     }
+//     catch(error){
+//         console.error("ERROR : ",error);
+//         throw error;
+//     }
+// })()
